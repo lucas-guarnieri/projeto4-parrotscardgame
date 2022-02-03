36f87c4e92032797556679878d5c9173afc439f3
LucasG <lucaslguarnieri@gmail.com>
2022-02-03 12:40:16 -0300

    Randomization of cards order add. Cards are created as per user entry

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 /*GLOBAL VARIABLES*/
 
-let numOfCArds = null;
+const imageList = [
+    "bobrossparrot.gif",
+    "explodyparrot.gif",
+    "fiestaparrot.gif",
+    "metalparrot.gif",
+    "revertitparrot.gif",
+    "tripletsparrot.gif",
+    "unicornparrot.gif"
+];
 
-getNumOfCards();
-function getNumOfCards(){
-    let num = prompt("Com quantas cartas você quer jogar?\nO número de cartas deve ser par e no máximo que 14");
+let numOfCards = null;
+let randomImageList = [];
+
+setNumOfCards();
+createRandomList();
+createCards();
+
+
+function setNumOfCards(){
+    let num = prompt("Com quantas cartas você quer jogar?\nO número de cartas deve ser par e no máximo 14");
     if (num % 2 == 0 && num <= 14){
-        numOfCArds = num;
-        
+        numOfCards = num;
     }
     else{
         alert("O NÚMERO de cartas DEVE ser PAR e MENOR que 14")
-        getNumOfCards();
+        setNumOfCards();
     }
 }
+
+function createRandomList(){
+    for (let i = 0; i < numOfCards/2; i++){
+        randomImageList.push(imageList[i]);
+        randomImageList.push(imageList[i]);
+    }
+    randomImageList =randomImageList.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+function createCards(){
+    const gameBoard = document.querySelector(".gameBoard");
+    for (let i = 0; i < randomImageList.length; i++){
+        gameBoard.innerHTML += `
+        <div class="card" data-identifier="card">
+            <div class="front-face face" data-identifier="front-face">
+                <img src="/images/front.png">
+            </div>
+            <div class="back-face face" data-identifier="back-face">
+                <img src="/images/${randomImageList[i]}">
+            </div>
+        </div>
+        `
+    }
+}
+
+
